f32cd473a026494e3f78f5bf5111cbfb4c88888d
berila7 <berilasimo7@gmail.com>
2026-09-02 19:05:03 +0100

    adding the conf and init for wordpress

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -4,8 +4,27 @@ set -e
 
 mkdir -p /var/www/html
 
-if [ ! -f "/var/www/html/index.php" ]; then
+if [ ! -f "/var/www/html/wp-config.php" ]; then
 
     echo "WordPress files not found. Installing..."
 
+    if [ ! -f "/var/www/html/index.php" ]; then
+        wp core download \
+            --path=/var/www/html \
+            --version=7.1 \
+            --allow-root
+
+    fi
+
+    DB_PASS=$(cat /run/secrets/db_password)
+
+    wp config create \
+        --path=/var/www/html \
+        --dbname="$MYSQL_DATABASE" \
+        --dbuser="$MYSQL_USER" \
+        --dbpass="$DB_PASS" \
+        --dbhost="$MYSQL_HOST" \
+        --allow-root
 fi
+
+exec php-fpm8.2 -F
