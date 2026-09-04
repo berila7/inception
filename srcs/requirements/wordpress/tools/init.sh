6c41b1b707fb6f79b539195bec8825813288ec9b
berila7 <berilasimo7@gmail.com>
2026-09-04 10:54:51 +0100

    wordpress completed

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -4,17 +4,17 @@ set -e
 
 mkdir -p /var/www/html
 
+if [ ! -f "/var/www/html/index.php" ]; then
+    echo "WordPress files not found. Downloading..."
+
+    wp core download \
+        --path=/var/www/html \
+        --version=7.1 \
+        --allow-root
+fi
+
 if [ ! -f "/var/www/html/wp-config.php" ]; then
-
-    echo "WordPress files not found. Installing..."
-
-    if [ ! -f "/var/www/html/index.php" ]; then
-        wp core download \
-            --path=/var/www/html \
-            --version=7.1 \
-            --allow-root
-
-    fi
+    echo "WordPress configuration not found. Creating..."
 
     DB_PASS=$(cat /run/secrets/db_password)
 
@@ -24,6 +24,24 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
         --dbuser="$MYSQL_USER" \
         --dbpass="$DB_PASS" \
         --dbhost="$MYSQL_HOST" \
+        --allow-root
+fi
+
+if ! HTTP_HOST="$DOMAIN_NAME" wp core is-installed \
+    --path=/var/www/html \
+    --allow-root
+then
+    echo "WordPress database not installed. Installing..."
+
+    WP_ADMIN_PASS=$(cat /run/secrets/wp_admin_password)
+
+    HTTP_HOST="$DOMAIN_NAME" wp core install \
+        --path=/var/www/html \
+        --url="https://$DOMAIN_NAME" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN_USER" \
+        --admin_password="$WP_ADMIN_PASS" \
+        --admin_email="$WP_ADMIN_EMAIL" \
         --allow-root
 fi
 
