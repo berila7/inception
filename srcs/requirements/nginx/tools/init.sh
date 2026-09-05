cd0b9e4944097a43b103f1e884c8e4e887a5ef75
berila7 <berilasimo7@gmail.com>
2026-09-05 12:21:58 +0100

    nginx finished

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -2,4 +2,17 @@
 
 set -e
 
+mkdir -p /etc/nginx/ssl
+
+if [ ! -f /etc/nginx/ssl/server.crt ] || [ ! -f /etc/nginx/ssl/server.key ]; then
+    echo "Generating TLS certificate..."
+    
+    openssl req -x509 -nodes -days 365 \
+        -newkey rsa:2048 \
+        -keyout /etc/nginx/ssl/server.key \
+        -out /etc/nginx/ssl/server.crt \
+        -subj "/C=MA/ST=Casablanca/L=Casablanca/O=42/OU=Student/CN=$DOMAIN_NAME"
+
+fi
+
 exec nginx -g "daemon off;"
